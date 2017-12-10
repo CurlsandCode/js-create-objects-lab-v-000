323260e3306d9517e1c42ff6208d2e4b03fa0590
Monika Williams <monika.m.williams@gmail.com>
2017-12-10 05:44:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ function Dog (name,breed,age) {
   this.breed = breed;
   this.age = age;
 }
+function Cat (name,breed,age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+function Ferret(name,age, foodRequirements) {
+  this.name = name;
+  this.breed = breed;
+  this.foodRequirements = foodRequirements;
+}
+function Bird (name,breed) {
+  this.name = name;
+  this.breed = breed;
+
+}
+function Fish (name,species,waterConditions) {
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
